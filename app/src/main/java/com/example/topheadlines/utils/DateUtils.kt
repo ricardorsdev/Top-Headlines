6c1bef18b371f5382a29ea1c83cb2d18af65e5be
Ricardo Sousa <sousa.ricardorodrigues@gmail.com>
2023-01-15 02:34:55 +0000

    fix: adding try/catch block to Date conversion, including date pattern on BuildConfig to deal with different date patterns among sources

--- app/src/main/java/com/example/topheadlines/utils/DateUtils.kt
+++ app/src/main/java/com/example/topheadlines/utils/DateUtils.kt
@@ -1,15 +1,21 @@
 package com.example.topheadlines.utils
 
+import com.example.topheadlines.BuildConfig
 import java.text.SimpleDateFormat
 import java.util.*
 
 object DateUtils {
-    private const val apiDateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    private const val apiDateFormat = BuildConfig.DATE_PATTERN
 
     fun convertStringDateToTime(stringDate: String?): Long {
         stringDate?.let {
-            val sdf = SimpleDateFormat(apiDateFormat, Locale.getDefault())
-            return sdf.parse(it)?.time ?: 0L
+            try {
+                val sdf = SimpleDateFormat(apiDateFormat, Locale.getDefault())
+                return sdf.parse(it)?.time ?: 0L
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return 0L
+            }
         }
         return 0L
     }
